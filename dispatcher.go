ca53ce117d23e8642eeb657c5330c393256cc28c
Mikhail Pak <mikhail.pak@tum.de>
2017-12-24 11:38:03 +0100

    Reorder loops

--- dispatcher.go
+++ dispatcher.go
@@ -16,40 +16,36 @@ func mergeMaps(primary map[string]string, secondary map[string]string) {
 }
 
 func ProcessFile(templateFilename string, send bool, c *Config) error {
-	// FIXME: Disentangle this mess
-
 	data, err := ioutil.ReadFile(templateFilename)
 	if err != nil {
 		return err
 	}
 	data_s := string(data)
 
+	ap := new(authPair)
 	if send {
 		fmt.Printf("Please enter your credentials for \"%s\"\n", c.EmailServer.Hostname)
-		ap := new(authPair)
 		ap.prompt()
+	}
 
-		for recipient, context := range c.Recipients {
-			mergeMaps(context, c.GlobalContext)
+	for recipient, localContext := range c.Recipients {
+		// Add global context to local context
+		mergeMaps(localContext, c.GlobalContext)
 
-			email := NewEmail().AddAuthor(&c.Author).AddRecipient(recipient).AddContent(data_s).Build(context)
+		email := NewEmail().AddAuthor(&c.Author).AddRecipient(recipient).AddContent(data_s).Build(localContext)
 
+		if send {
 			fmt.Printf("Will send to %v\n", email.GetRecipients())
 			if err := email.Send(&c.EmailServer, ap); err != nil {
 				return err
 			}
-		}
-	} else {
-		for recipient, context := range c.Recipients {
-			mergeMaps(context, c.GlobalContext)
-
-			email := NewEmail().AddAuthor(&c.Author).AddRecipient(recipient).AddContent(data_s).Build(context)
-
+		} else {
 			fmt.Printf("Send flag not set: opening preview in \"%s\"\n", c.Author.Browser)
 			if err := email.OpenInBrowser(c.Author.Browser); err != nil {
 				return err
 			}
 		}
 	}
+
 	return nil
 }
