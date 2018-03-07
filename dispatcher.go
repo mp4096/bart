5fe9313c99fe226983b27903610f4c079f42ca53
Mikhail Pak <mikhail.pak@tum.de>
2018-03-07 12:23:07 +0100

    Add context verification
    * A context MUST contain the key/tag 'subject'
    * A context MAY NOT contain any keys/tags beginning
    with an underscore

--- dispatcher.go
+++ dispatcher.go
@@ -1,9 +1,14 @@
 package bart
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"regexp"
 )
+
+// Matches an underscore at the beginning of the string
+var underscoreRegex = regexp.MustCompile(`^\_`)
 
 /// Merge two maps. Key-value pairs from `secondary` are added to primary.
 /// If a key is present in both maps, the `primary`'s value is retained.
@@ -13,6 +18,24 @@ func mergeMaps(primary map[string]string, secondary map[string]string) {
 			primary[k] = v
 		}
 	}
+}
+
+/// Verify a context. Key 'subject' must be present.
+func checkForSubject(context map[string]string) error {
+	if _, ok := context["subject"]; ok {
+		return nil
+	}
+	return errors.New("No 'subject' tag found. It must be contained in the context.")
+}
+
+/// Verify a context. No keys beginning with an underscore are allowed.
+func checkForNoUnderscores(context map[string]string) error {
+	for k, _ := range context {
+		if underscoreRegex.MatchString(k) {
+			return errors.New("Tags beginning with an underscore are not allowed.")
+		}
+	}
+	return nil
 }
 
 func ProcessFile(templateFilename string, send bool, c *Config) error {
@@ -31,6 +54,13 @@ func ProcessFile(templateFilename string, send bool, c *Config) error {
 	for recipient, localContext := range c.Recipients {
 		// Add global context to local context
 		mergeMaps(localContext, c.GlobalContext)
+
+		if err := checkForSubject(localContext); err != nil {
+			return err
+		}
+		if err := checkForNoUnderscores(localContext); err != nil {
+			return err
+		}
 
 		email := NewEmail().AddAuthor(&c.Author).AddRecipient(recipient).AddContent(data_s).Build(localContext)
 
