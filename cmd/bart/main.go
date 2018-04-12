70a0ec2e69d555b854013e7d2a16c59bb9f40a1d
Mikhail Pak <mikhail.pak@tum.de>
2018-04-12 10:57:53 +0200

    Rename return value enum constants

--- cmd/bart/main.go
+++ cmd/bart/main.go
@@ -9,12 +9,11 @@ import (
 )
 
 const (
-	OK                         int = 0
-	INVALID_COMMAND            int = 1
-	NO_CONFIG_FILE_SPECIFIED   int = 3
-	NO_TEMPLATE_FILE_SPECIFIED int = 4
-	COULD_NOT_OPEN_CONFIG_FILE int = 5
-	COULD_NOT_SEND_OR_PREVIEW  int = 6
+	ok                      int = 0
+	noConfigFileSpecified   int = 3
+	noTemplateFileSpecified int = 4
+	couldNotOpenConfigFile  int = 5
+	couldNotSendOrPreview   int = 6
 )
 
 var send = false
@@ -32,19 +31,19 @@ func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("bart sends templated emails for you")
 		fmt.Println("Run bart -h for help")
-		os.Exit(OK)
+		os.Exit(ok)
 	}
 
 	fs.Parse(os.Args[1:])
 
 	if len(configFilename) == 0 {
 		fmt.Println("Config file not specified")
-		os.Exit(NO_CONFIG_FILE_SPECIFIED)
+		os.Exit(noConfigFileSpecified)
 	}
 
 	if len(templateFilename) == 0 {
 		fmt.Println("Template file not specified")
-		os.Exit(NO_TEMPLATE_FILE_SPECIFIED)
+		os.Exit(noTemplateFileSpecified)
 	}
 
 	c := new(bart.Config)
@@ -52,7 +51,7 @@ func main() {
 	if errConf != nil {
 		fmt.Println("Error opening config file")
 		fmt.Println(errConf)
-		os.Exit(COULD_NOT_OPEN_CONFIG_FILE)
+		os.Exit(couldNotOpenConfigFile)
 	}
 
 	fmt.Println("Hello,", c.Author.Name)
@@ -60,6 +59,6 @@ func main() {
 	if err := bart.ProcessFile(templateFilename, send, c); err != nil {
 		fmt.Println("Error while sending email or opening preview")
 		fmt.Println(err)
-		os.Exit(COULD_NOT_SEND_OR_PREVIEW)
+		os.Exit(couldNotSendOrPreview)
 	}
 }
